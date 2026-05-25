65d370419fca451e8e2859a3a87fec4c850306e4
KarimShamel <karimshamel88@gmail.com>
2026-05-25 17:21:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,72 @@
+const lever = document.getElementById('leverContainer');
+
+  setTimeout(() => {
+    reel3.style.transform = 'translateY(-120px)';
+  }, 800);
+}
+
+calendarBtn.addEventListener('click', () => {
+
+  const title = 'Ehab and Habiba Wedding';
+  const location = 'Royal Palace Ballroom, Cairo';
+  const details = 'Join us for our wedding celebration!';
+
+  const startDate = '20260922T190000';
+  const endDate = '20260922T230000';
+
+  const googleCalendarUrl = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}&dates=${startDate}/${endDate}&details=${encodeURIComponent(details)}&location=${encodeURIComponent(location)}`;
+
+  window.open(googleCalendarUrl, '_blank');
+});
+
+function createConfetti() {
+
+  for (let i = 0; i < 120; i++) {
+
+    const confetti = document.createElement('div');
+
+    confetti.classList.add('confetti');
+
+    confetti.style.left = Math.random() * window.innerWidth + 'px';
+    confetti.style.top = '-20px';
+
+    confetti.style.animationDuration = (Math.random() * 3 + 2) + 's';
+
+    confetti.style.opacity = Math.random();
+
+    confetti.style.transform = `rotate(${Math.random() * 360}deg)`;
+
+    confetti.style.background = Math.random() > 0.5
+      ? '#d4af37'
+      : '#ffffff';
+
+    confetti.style.width = Math.random() * 10 + 6 + 'px';
+    confetti.style.height = Math.random() * 10 + 6 + 'px';
+
+    document.body.appendChild(confetti);
+
+    setTimeout(() => {
+      confetti.remove();
+    }, 5000);
+  }
+}
+
+const style = document.createElement('style');
+style.innerHTML = `
+
+.confetti {
+  position: fixed;
+  z-index: 999;
+  border-radius: 3px;
+  animation: fall linear forwards;
+}
+
+@keyframes fall {
+  to {
+    transform: translateY(110vh) rotate(720deg);
+  }
+}
+
+`;
+
+document.head.appendChild(style);
